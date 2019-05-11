04e0d051dc5ecc0aee4bda080ba8cf3e9faa97b4
iMoD1998 <imod1998@gmail.com>
2019-05-11 16:38:20 +0100

    Cluster fuck v4
    It's working tho

--- xbRevive/Main.cpp
+++ xbRevive/Main.cpp
@@ -2,6 +2,7 @@
 
 #include <Windows.h>
 #include <stdio.h>
+#include <chrono>
 
 typedef unsigned long long QWORD;
 
@@ -55,11 +56,21 @@ typedef unsigned long long QWORD;
 #define STATUS_BUSY         	0x1u			//Busy
 #define STATUS_ERROR			(STATUS_ILL_LOG|STATUS_ADDR_ER|STATUS_BB_ER|STATUS_RNP_ER|STATUS_ECC_ER|STATUS_WR_ER)
 
+//Jasper 200 Pages 400 Blocks 4KB block 0x1000000  0x000231D4 
+
+//Corona 0x00043000
+
+//Xenon 0x01198010 16MB
+
+//Falcon 0x01198010 16MB
+
+//Zepher 0x01198030 16MB
+
 //Jasper 16 MB NAND WITH CONFIG 0x00023010 
 
 //Jasper 256 MB NAND WITH CONFIG 0x008A3020 
 
-//Jasper 512 MB NAND WITH CONFIG 0xAA3020 
+//Jasper 512 MB NAND WITH CONFIG 0x00AA3020 
 //0x21000 PHYSICAL BLOCK SIZE
 //0x210 PHYSICAL PAGE SIZE
 //PHYSICAL = PAGE DATA + META DATA (ECC)
@@ -77,47 +88,47 @@ typedef unsigned long long QWORD;
 #define JASPER_LOGICAL_PAGE_SIZE 0x200
 #define JASPER_LOGICAL_PAGES_PER_BLOCK (JASPER_LOGICAL_BLOCK_SIZE / JASPER_LOGICAL_PAGE_SIZE)
 
-BOOL _inline SPIBegin( HANDLE Sidecar )
+BOOL _inline SPIBegin(HANDLE Sidecar)
 {
-	if ( XSidecarEmulatorSpiBegin( Sidecar ) == 0 )
+	if (XSidecarEmulatorSpiBegin(Sidecar) == 0)
 	{
-		printf( "XSidecarEmulatorSpiBegin: Failed\n" );
+		printf("XSidecarEmulatorSpiBegin: Failed\n");
 		return FALSE;
 	}
 
 	return TRUE;
 }
 
-BOOL _inline SPIEnd( HANDLE Sidecar )
+BOOL _inline SPIEnd(HANDLE Sidecar)
 {
-	if ( XSidecarEmulatorSpiEnd( Sidecar ) == 0 )
+	if (XSidecarEmulatorSpiEnd(Sidecar) == 0)
 	{
-		printf( "XSidecarEmulatorSpiEnd: Failed\n" );
+		printf("XSidecarEmulatorSpiEnd: Failed\n");
 		return FALSE;
 	}
 
 	return TRUE;
 }
 
-DWORD ReadSPIReg( HANDLE Sidecar, BYTE Register )
+DWORD ReadSPIReg(HANDLE Sidecar, BYTE Register)
 {
-	BYTE ReadPacket[ 2 ] = {
-		( Register << 2 ) | 1,
+	BYTE ReadPacket[2] = {
+		(Register << 2) | 1,
 		0xC1,
 	};
 
-	if ( XSidecarEmulatorSpiWrite( Sidecar, ReadPacket, sizeof( ReadPacket ) ) == 0 )
+	if (XSidecarEmulatorSpiWrite(Sidecar, ReadPacket, sizeof(ReadPacket)) == 0)
 	{
-		printf( "XSidecarEmulatorSpiWrite: Failed\n" );
+		printf("XSidecarEmulatorSpiWrite: Failed\n");
 		return 0;
 	}
 
 	DWORD Buffer = 0;
 	DWORD BytesRead = 0;
 
-	if ( XSidecarEmulatorSpiRead( Sidecar, (PBYTE)&Buffer, 4, &BytesRead ) == 0 )
+	if (XSidecarEmulatorSpiRead(Sidecar, (PBYTE)& Buffer, 4, &BytesRead) == 0)
 	{
-		printf( "XSidecarEmulatorSpiRead: Failed\n" );
+		printf("XSidecarEmulatorSpiRead: Failed\n");
 
 		return 0;
 	}
@@ -125,10 +136,10 @@ DWORD ReadSPIReg( HANDLE Sidecar, BYTE Register )
 	return Buffer;
 }
 
-BOOL WriteSPIReg( HANDLE Sidecar, BYTE Register, DWORD Data )
+BOOL WriteSPIReg(HANDLE Sidecar, BYTE Register, DWORD Data)
 {
-	BYTE WritePacket[ 6 ] = {
-		( Register << 2 ) | 2,
+	BYTE WritePacket[6] = {
+		(Register << 2) | 2,
 		7,
 		0,
 		0,
@@ -136,271 +147,305 @@ BOOL WriteSPIReg( HANDLE Sidecar, BYTE Register, DWORD Data )
 		0
 	};
 
-	*(DWORD*)&WritePacket[ 2 ] = ( Data );
+	*(DWORD*)& WritePacket[2] = (Data);
 
-	if ( XSidecarEmulatorSpiWrite( Sidecar, WritePacket, sizeof( WritePacket ) ) == 0 )
+	if (XSidecarEmulatorSpiWrite(Sidecar, WritePacket, sizeof(WritePacket)) == 0)
 	{
-		printf( "XSidecarEmulatorSpiWrite: Failed\n" );
+		printf("XSidecarEmulatorSpiWrite: Failed\n");
 		return 0;
 	}
 
 	return TRUE;
 }
 
-DWORD _inline GetStatus( HANDLE Sidecar )
+struct BitMaskThing
 {
-	return ReadSPIReg( Sidecar, SFCX_STATUS );
+	const char* Name;
+	DWORD Mask;
+};
+
+void PrintStatus(DWORD Status)
+{
+	printf("Status: %08X ", Status);
+
+	BitMaskThing Things[] = {
+		{ "STATUS_BUSY", STATUS_BUSY },
+		{ "STATUS_WR_ER", STATUS_WR_ER },
+		{ "STATUS_ECC_ER", STATUS_ECC_ER },
+		{ "STATUS_RNP_ER", STATUS_RNP_ER },
+		{ "STATUS_BB_ER", STATUS_BB_ER },
+		{ "STATUS_ADDR_ER", STATUS_ADDR_ER },
+		{ "STATUS_INT_CP", STATUS_INT_CP },
+		{ "STATUS_PIN_BY_N", STATUS_PIN_BY_N },
+		{ "STATUS_PIN_WP_N", STATUS_PIN_WP_N },
+		{ "STATUS_ILL_LOG", STATUS_ILL_LOG },
+
+	};
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (Status & Things[i].Mask)
+		{
+			printf("%s |", Things[i].Name);
+		}
+	}
+
+	printf("\n");
 }
 
-DWORD _inline GetConfig( HANDLE Sidecar )
+DWORD _inline GetStatus(HANDLE Sidecar)
 {
-	return ReadSPIReg( Sidecar, SFCX_CONFIG );
+	return ReadSPIReg(Sidecar, SFCX_STATUS);
 }
 
-void WaitWhileBusy( HANDLE Sidecar )
+DWORD _inline GetConfig(HANDLE Sidecar)
 {
-
+	return ReadSPIReg(Sidecar, SFCX_CONFIG);
 }
 
-void SPIDoCommand( HANDLE Sidecar, BYTE Command )
+void WaitWhileBusy(HANDLE Sidecar)
 {
-	WriteSPIReg( Sidecar, SFCX_COMMAND, Command );
+	DWORD Status = 0;
 
-	WaitWhileBusy( Sidecar );
+	do
+	{
+		Status = GetStatus(Sidecar);
+	} while (Status & STATUS_BUSY);
 }
 
-DWORD _inline GetBlockSize( BOOL Logical )
+void SPIDoCommand(HANDLE Sidecar, BYTE Command)
+{
+	WriteSPIReg(Sidecar, SFCX_COMMAND, Command);
+
+	if (Command == LOG_PAGE_TO_BUF || Command == PHY_PAGE_TO_BUF || Command == WRITE_PAGE_TO_PHY || Command == BLOCK_ERASE)
+	{
+		WaitWhileBusy(Sidecar);
+	}
+}
+
+DWORD _inline GetBlockSize(BOOL Logical)
 {
 	return Logical ? JASPER_LOGICAL_BLOCK_SIZE : JASPER_PHYSICAL_BLOCK_SIZE;
 }
 
-DWORD _inline GetPageSize( BOOL Logical )
+DWORD _inline GetPageSize(BOOL Logical)
 {
 	return Logical ? JASPER_LOGICAL_PAGE_SIZE : JASPER_PHYSICAL_PAGE_SIZE;
 }
 
-DWORD _inline GetPagesPerBlock( BOOL Logical )
+DWORD _inline GetPagesPerBlock(BOOL Logical)
 {
 	return Logical ? JASPER_LOGICAL_PAGES_PER_BLOCK : JASPER_PHYSICAL_PAGES_PER_BLOCK;
 }
 
-DWORD _inline IndexToAddress( DWORD BlockIndex, DWORD PageIndex, BOOL Logical )
+DWORD _inline IndexToAddress(DWORD BlockIndex, DWORD PageIndex, BOOL Logical)
 {
-	return ( BlockIndex * JASPER_LOGICAL_BLOCK_SIZE ) + ( PageIndex * JASPER_LOGICAL_PAGE_SIZE );
+	//return ( BlockIndex * JASPER_LOGICAL_BLOCK_SIZE ) + ( PageIndex * JASPER_LOGICAL_PAGE_SIZE );
+
+	return (BlockIndex * JASPER_LOGICAL_BLOCK_SIZE) + (PageIndex * JASPER_LOGICAL_PAGE_SIZE);
 }
 
-DWORD ReadPage( HANDLE Sidecar, DWORD Address, BYTE* Buffer, BOOL Logical )
+void ReadPage(HANDLE Sidecar, DWORD Address, BYTE* Buffer, BOOL Logical)
 {
-	DWORD PageSize = GetPageSize( Logical );
+	WriteSPIReg(Sidecar, SFCX_ADDRESS, Address);
 
-	WriteSPIReg( Sidecar, SFCX_ADDRESS, Address );
+	SPIDoCommand(Sidecar, Logical ? LOG_PAGE_TO_BUF : PHY_PAGE_TO_BUF);
 
-	SPIDoCommand( Sidecar, Logical ? LOG_PAGE_TO_BUF : PHY_PAGE_TO_BUF );
+	WriteSPIReg(Sidecar, SFCX_ADDRESS, 0);
 
-	WriteSPIReg( Sidecar, SFCX_ADDRESS, 0 );
-
-	for ( int i = 0; i < PageSize; i += 4 )
+	for (int i = 0; i < GetPageSize( Logical ); i += 4)
 	{
-		SPIDoCommand( Sidecar, PAGE_BUF_TO_REG ); // Move page buffer to data register
-		*(DWORD*)( &Buffer[ i ] ) = ReadSPIReg( Sidecar, SFCX_DATA );  // get contents of dataregister
+		SPIDoCommand(Sidecar, PAGE_BUF_TO_REG); // Move page buffer to data register
+		*(DWORD*)(&Buffer[i]) = ReadSPIReg(Sidecar, SFCX_DATA);  // get contents of dataregister
 	}
-
-	return 0;
 }
 
-DWORD ReadPage( HANDLE Sidecar, DWORD BlockIndex, DWORD PageIndex, BYTE* Buffer, BOOL Logical )
+void ReadPage(HANDLE Sidecar, DWORD BlockIndex, DWORD PageIndex, BYTE* Buffer, BOOL Logical)
 {
-	if ( PageIndex >= GetPagesPerBlock( Logical ) )
-	{
-		printf( "[ERROR] Invalid Page ID\n" );
-	}
-
-	DWORD Address = IndexToAddress( BlockIndex, PageIndex, Logical );
+	DWORD Address = IndexToAddress(BlockIndex, PageIndex, Logical);
 
 	//printf( "Reading Page: %i/%i Address ( %08X )\n", PageIndex, GetPagesPerBlock( Logical ), Address );
 
-	return ReadPage( Sidecar, Address, Buffer, Logical );
+	ReadPage(Sidecar, Address, Buffer, Logical);
 }
 
-DWORD ReadBlock( HANDLE Sidecar, DWORD BlockID, BYTE* Buffer, BOOL Logical )
+void ReadBlock(HANDLE Sidecar, DWORD BlockID, BYTE* Buffer, BOOL Logical)
 {
-	for ( int i = 0; i < GetPagesPerBlock( Logical ); ++i )
+	for (int i = 0; i < GetPagesPerBlock(Logical); ++i)
 	{
-		DWORD Offset = GetPageSize( Logical ) * i;
+		ReadPage(Sidecar, BlockID, i, &Buffer[ GetPageSize( Logical ) * i ], Logical);
+	}
+}
 
-		DWORD Status = ReadPage( Sidecar, BlockID, i, &Buffer[ Offset ], Logical );
+void WritePage(HANDLE Sidecar, DWORD Address, BYTE* Buffer, BOOL Logical)
+{
+	DWORD Config = ReadSPIReg(Sidecar, SFCX_CONFIG);
+
+	WriteSPIReg(Sidecar, SFCX_CONFIG, Config | CONFIG_WP_EN);
+
+	WriteSPIReg(Sidecar, SFCX_ADDRESS, 0);
+
+	for (int i = 0; i < GetPageSize( Logical ); i += 4)
+	{
+		WriteSPIReg(Sidecar, SFCX_DATA, *(DWORD*)(&Buffer[i]));  // set contents of dataregister
+		SPIDoCommand(Sidecar, REG_TO_PAGE_BUF); // Move data register to page buffer
 	}
 
-	return 0;
+	WriteSPIReg(Sidecar, SFCX_ADDRESS, Address);
+
+	SPIDoCommand(Sidecar, UNLOCK_CMD_0);
+
+	SPIDoCommand(Sidecar, UNLOCK_CMD_1);
+
+	SPIDoCommand(Sidecar, WRITE_PAGE_TO_PHY);
 }
 
-DWORD WritePage( HANDLE Sidecar, DWORD Address, BYTE* Buffer, BOOL Logical )
+void WritePage(HANDLE Sidecar, DWORD BlockIndex, DWORD PageIndex, BYTE* Buffer, BOOL Logical)
 {
-	DWORD PageSize = GetPageSize( Logical );
-
-	if ( Address % PageSize != 0 )
+	if (PageIndex >= GetPagesPerBlock(Logical))
 	{
-		printf( "[ERROR] Misaligned Address\n" );
-		return 0;
+		printf("[ERROR] Invalid Page ID\n");
 	}
 
-	WriteSPIReg( Sidecar, SFCX_ADDRESS, 0 );
+	auto Address = IndexToAddress(BlockIndex, PageIndex, Logical);
 
-	for ( int i = 0; i < PageSize; i += 4 )
-	{
-		WriteSPIReg( Sidecar, SFCX_DATA, *(DWORD*)( &Buffer[ i ] ) );  // set contents of dataregister
-		SPIDoCommand( Sidecar, REG_TO_PAGE_BUF ); // Move data register to page buffer
-	}
-
-	SPIDoCommand( Sidecar, UNLOCK_CMD_0 );
-
-	SPIDoCommand( Sidecar, UNLOCK_CMD_1 );
-
-	SPIDoCommand( Sidecar, WRITE_PAGE_TO_PHY );
-
-	return GetStatus( Sidecar );
+	WritePage(Sidecar, Address, Buffer, Logical);
 }
 
-DWORD WritePage( HANDLE Sidecar, DWORD BlockIndex, DWORD PageIndex, BYTE* Buffer, BOOL Logical )
+void EraseBlock(HANDLE Sidecar, DWORD BlockID)
 {
-	if ( PageIndex >= GetPagesPerBlock( Logical ) )
-	{
-		printf( "[ERROR] Invalid Page ID\n" );
-	}
+	DWORD Config = ReadSPIReg(Sidecar, SFCX_CONFIG);
 
-	DWORD Address = IndexToAddress( BlockIndex, PageIndex, Logical );
+	WriteSPIReg(Sidecar, SFCX_CONFIG, Config | CONFIG_WP_EN);
 
-	return WritePage( Sidecar, Address, Buffer, Logical );
+	WriteSPIReg(Sidecar, SFCX_ADDRESS, IndexToAddress(BlockID, 0, FALSE));
+
+	SPIDoCommand(Sidecar, UNLOCK_CMD_1);
+	SPIDoCommand(Sidecar, UNLOCK_CMD_0);
+
+	SPIDoCommand(Sidecar, BLOCK_ERASE);
 }
 
-DWORD EraseBlock( HANDLE Sidecar, DWORD BlockID )
+void WriteBlock(HANDLE Sidecar, DWORD BlockID, BYTE * Buffer, BOOL Logical)
 {
-	WriteSPIReg( Sidecar, SFCX_ADDRESS, IndexToAddress( BlockID, 0, FALSE ) );
+	EraseBlock(Sidecar, BlockID);
 
-	SPIDoCommand( Sidecar, UNLOCK_CMD_1 );
-	SPIDoCommand( Sidecar, UNLOCK_CMD_0 );
-
-	WriteSPIReg( Sidecar, SFCX_COMMAND, BLOCK_ERASE );
-
-	return GetStatus( Sidecar );
-}
-
-DWORD WriteBlock( HANDLE Sidecar, DWORD BlockID, BYTE* Buffer, BOOL Logical )
-{
-	if ( Logical )
-		return STATUS_ERROR; // Not done yet
-
-	EraseBlock( Sidecar, BlockID );
-
-	for ( int i = 0; i < GetPagesPerBlock( Logical ); ++i )
+	for (int i = 0; i < GetPagesPerBlock(Logical); ++i)
 	{
-		DWORD Offset = IndexToAddress( 0, i, Logical );
+		DWORD Offset = GetPageSize(Logical) * i;
 
-		DWORD Status = WritePage( Sidecar, BlockID, i, &Buffer[ Offset ], Logical );
+		WritePage(Sidecar, BlockID, i, &Buffer[Offset], Logical);
 	}
-
-	return GetStatus( Sidecar );
 }
 
-void ReadNAND( HANDLE Sidecar )
+void ReadNAND(HANDLE Sidecar)
 {
 	FILE* DumpFile;
 
-	if ( fopen_s( &DumpFile, "Nand.bin", "wb+" ) == 0 )
+	if (fopen_s(&DumpFile, "Nand.bin", "wb+") == 0)
 	{
-		unsigned int config = ReadSPIReg( Sidecar, SFCX_CONFIG );
+		unsigned int config = ReadSPIReg(Sidecar, SFCX_CONFIG);
 
-		WriteSPIReg( Sidecar, SFCX_CONFIG, config & ~( CONFIG_INT_EN | CONFIG_WP_EN | CONFIG_DMA_LEN ) );
+		DWORD NumBlocks = 20;
 
-		DWORD NumBlocks = 10;
+		printf("Reading blocks:  Logical      Physical\n");
+		printf("                 ==========   ==========\n");
 
-		printf( "Reading blocks:  Logical      Physical\n" );
-		printf( "                 ==========   ==========\n" );
-
-		for ( int i = 0; i < NumBlocks; i++ )
+		for (int i = 0; i < NumBlocks; i++)
 		{
-			DWORD Logical = IndexToAddress( i, 0, TRUE );
-			DWORD Physical = IndexToAddress( i, 0, FALSE );
+			DWORD Logical = IndexToAddress(i, 0, TRUE);
+			DWORD Physical = IndexToAddress(i, 0, FALSE);
 			DWORD CurrentBlockNum = i + 1;
 
-			double PercentDone = ( (double)CurrentBlockNum / (double)NumBlocks ) * 100.0;
+			double PercentDone = ((double)CurrentBlockNum / (double)NumBlocks) * 100.0;
 
-			printf( "                 0x%08X - 0x%08X          (%3.2f%%)\n", Logical, Physical, PercentDone );
+			printf("                 0x%08X - 0x%08X          (%3.2f%%)\n", Logical, Physical, PercentDone);
 
-			BYTE Block[ JASPER_PHYSICAL_BLOCK_SIZE ];
+			BYTE Block[JASPER_PHYSICAL_BLOCK_SIZE];
 
-			ReadBlock( Sidecar, i, Block, FALSE );
+			ReadBlock(Sidecar, i, Block, FALSE);
 
-			fwrite( Block, 1, JASPER_PHYSICAL_BLOCK_SIZE, DumpFile );
+			fwrite(Block, 1, JASPER_PHYSICAL_BLOCK_SIZE, DumpFile);
 		}
 
-		printf( "Finished\n" );
+		printf("Finished\n");
 
-		fclose( DumpFile );
+		fclose(DumpFile);
 	}
 	else
 	{
-		printf( "Failed To Open File\n" );
+		printf("Failed To Open File\n");
 	}
 }
 
-void FlashNand( HANDLE Sidecar )
+void FlashNand(HANDLE Sidecar)
 {
+	while (true);
+
 	FILE* FlashFile;
 
-	if ( fopen_s( &FlashFile, "NandToFlash.bin", "wb+" ) == 0 )
+	if (fopen_s(&FlashFile, "NandToFlash.bin", "rb+") == 0)
 	{
-		fseek( FlashFile, 0, SEEK_END ); // seek to end of file
+		fseek(FlashFile, 0, SEEK_END); // seek to end of file
 
-		SIZE_T Size = ftell( FlashFile ); // get current file pointer
+		SIZE_T Size = ftell(FlashFile); // get current file pointer
 
-		fseek( FlashFile, 0, SEEK_SET ); // seek back to beginning of file
+		fseek(FlashFile, 0, SEEK_SET); // seek back to beginning of file
 
-		WriteSPIReg( Sidecar, SFCX_CONFIG, ReadSPIReg( Sidecar, SFCX_CONFIG ) | CONFIG_WP_EN );
+		WriteSPIReg(Sidecar, SFCX_CONFIG, ReadSPIReg(Sidecar, SFCX_CONFIG) | CONFIG_WP_EN);
 
-		if ( Size % JASPER_PHYSICAL_BLOCK_SIZE != 0 )
+		if (Size % JASPER_PHYSICAL_BLOCK_SIZE != 0)
 		{
-			printf( "File Misaligned to blocks" );
+			printf("File Misaligned to blocks");
 			return;
 		}
 
-		if ( Size % JASPER_PHYSICAL_PAGE_SIZE != 0 )
+		if (Size % JASPER_PHYSICAL_PAGE_SIZE != 0)
 		{
-			printf( "File Misaligned to pages" );
+			printf("File Misaligned to pages");
 			return;
 		}
+
+		//yesn'twhomth
 
 		DWORD NumBlocks = Size / JASPER_PHYSICAL_BLOCK_SIZE;
 
-		printf( "Writing blocks:  Logical      Physical\n" );
-		printf( "                 ==========   ==========\n" );
+		printf("Writing blocks:  Logical      Physical\n");
+		printf("                 ==========   ==========\n");
 
-		for ( int i = 0; i < NumBlocks; i++ )
+		for (int i = 4; i < NumBlocks; i++)
 		{
-			DWORD Logical = IndexToAddress( i, 0, TRUE );
-			DWORD Physical = IndexToAddress( i, 0, FALSE );
+			DWORD Logical = IndexToAddress(i, 0, TRUE);
+			DWORD Physical = IndexToAddress(i, 0, FALSE);
 			DWORD CurrentBlockNum = i + 1;
 
-			double PercentDone = ( (double)CurrentBlockNum / (double)NumBlocks ) * 100.0;
+			double PercentDone = ((double)CurrentBlockNum / (double)NumBlocks) * 100.0;
 
-			printf( "                 0x%08X - 0x%08X          (%3.2f%%)\n", Logical, Physical, PercentDone );
+			printf("                 0x%08X - 0x%08X          (%3.2f%%)\n", Logical, Physical, PercentDone);
 
-			BYTE BlockBuffer[ JASPER_PHYSICAL_BLOCK_SIZE ];
+			BYTE BlockBuffer[JASPER_PHYSICAL_BLOCK_SIZE];
 
-			fread( BlockBuffer, 1, JASPER_PHYSICAL_BLOCK_SIZE, FlashFile );
+			memset(BlockBuffer, 0x33, JASPER_PHYSICAL_BLOCK_SIZE);
 
-			WriteBlock( Sidecar, i, BlockBuffer, FALSE );
+			fseek(FlashFile, i * JASPER_PHYSICAL_BLOCK_SIZE, SEEK_SET);
+			fread(BlockBuffer, 1, JASPER_PHYSICAL_BLOCK_SIZE, FlashFile);
+
+			WriteBlock(Sidecar, i, BlockBuffer, FALSE);
 		}
 
-		printf( "Flashing Done\n" );
+		printf("Flashing Done\n");
 	}
 	else
 	{
-		printf( "Couldnt Open NandToFlash.bin\n" );
+		printf("Couldnt Open NandToFlash.bin\n");
 	}
 }
 
-int main( int argc, const char* argv[] )
+// Yo nigga its ya boi
+//imod 1998 is like a good
+//yo wtf bro this is mad
+
+int main(int argc, const char* argv[])
 {
 	printf(
 		"SabMoDsoft (R) Xbox 360 Flash Tool (2.0.3206.0)\n"
@@ -408,70 +453,70 @@ int main( int argc, const char* argv[] )
 		"\n"
 	);
 
-	if ( LoadXSidecar( ) )
+	if (LoadXSidecar())
 	{
-		auto List = XSidecarOpenEmulatorList( );
-		auto ListCount = XSidecarGetListItemCount( List );
+		auto List = XSidecarOpenEmulatorList();
+		auto ListCount = XSidecarGetListItemCount(List);
 
-		printf( "List Count: %i\n", ListCount );
+		printf("List Count: %i\n", ListCount);
 
-		if ( ListCount == 0 )
+		if (ListCount == 0)
 		{
-			printf( "No sidecar detected\n" );
+			printf("No sidecar detected\n");
 			return 0;
 		}
 
-		WCHAR ConsoleNameBuffer[ 0x200 ];
+		WCHAR ConsoleNameBuffer[0x200];
 
-		auto Sidecar = XSidecarOpenListItem( List, 0 );
+		auto Sidecar = XSidecarOpenListItem(List, 0);
 
-		XSidecarGetName( Sidecar, ConsoleNameBuffer, 0x200, 0 );
+		XSidecarGetName(Sidecar, ConsoleNameBuffer, 0x200, 0);
 
-		printf( "Connected To %ws\n", ConsoleNameBuffer );
+		printf("Connected To %ws\n", ConsoleNameBuffer);
 
 		BOOL Power = FALSE;
 
-		XSidecarEmulatorGetPowerState( Sidecar, &Power );
+		XSidecarEmulatorGetPowerState(Sidecar, &Power);
 
-		if ( Power == TRUE )
+		//if ( Power == TRUE )
 		{
-			printf( "Powering down %ws\n", ConsoleNameBuffer );
-			XSidecarEmulatorSetPowerState( Sidecar, FALSE );
-			Sleep( 3500 );
+			printf("Powering down %ws\n", ConsoleNameBuffer);
+			XSidecarEmulatorSetPowerState(Sidecar, FALSE);
+			Sleep(3500);
 		}
 
-		printf( "Entering Flash Mode...\n" );
+		printf("Entering Flash Mode...\n");
 
 		//SPIEnd( Sidecar );
 
-		if ( SPIBegin( Sidecar ) )
+		if (SPIBegin(Sidecar))
 		{
-			printf( "Entered Flash Mode\n" );
+			printf("Entered Flash Mode\n");
 
-			ReadNAND( Sidecar );
+			ReadNAND(Sidecar);
 
-			printf( "Leaving Flash Mode...\n" );
+			printf("Leaving Flash Mode...\n");
 
-			if ( SPIEnd( Sidecar ) )
+			if (SPIEnd(Sidecar))
 			{
-				printf( "Left Flash Mode...\n" );
+				printf("Left Flash Mode...\n");
 			}
 			else
 			{
-				printf( "Could Not Leave Flash Mode\n" );
+				printf("Could Not Leave Flash Mode\n");
 			}
 		}
 		else
 		{
-			printf( "Failed To Enter Flashing Mode\n" );
+			printf("Failed To Enter Flashing Mode\n");
 		}
 
-		XSidecarCloseList( List );
+		XSidecarCloseList(List);
 	}
 
-	UnloadXSidecar( );
+	UnloadXSidecar();
 
-	getchar( );
+	getchar();
 
 	return 0;
 }
